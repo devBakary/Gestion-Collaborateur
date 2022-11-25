65094eab707a8758ceeb11e8543e4513a3ca031d
bddiakite <ffffffffff>
2022-11-25 21:57:50 +0000

    oauth configuration partielle

--- src/main/java/com/example/collaborateurgestion/Configuration/WebSecurityConfig.java
+++ src/main/java/com/example/collaborateurgestion/Configuration/WebSecurityConfig.java
@@ -54,11 +54,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable()
-                .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                //exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
+                //.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests().antMatchers("/api/auth/**").permitAll()
                 .antMatchers("/api/test/**").permitAll()
-                .anyRequest().authenticated();
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .and()
+                .oauth2Login();
 
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
     }
--- src/main/java/com/example/collaborateurgestion/Controller/TestController.java
+++ src/main/java/com/example/collaborateurgestion/Controller/TestController.java
@@ -2,20 +2,31 @@ package com.example.collaborateurgestion.Controller;
 
 import com.example.collaborateurgestion.Model.User;
 import com.example.collaborateurgestion.Repository.UserRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.util.List;
+import java.util.Map;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
-@RequestMapping("/api/test")
+//@RequestMapping("/api/test")
+@AllArgsConstructor
 public class TestController {
 
+    private final OAuth2AuthorizedClientService authorizedClientService;
 
     @Autowired
     UserRepository userRepository;
+
 
     @GetMapping("/user")
     @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
@@ -23,11 +34,48 @@ public class TestController {
         return "User Content.";
     }
 
-   /* @GetMapping("/mod")
-    @PreAuthorize("hasRole('MODERATOR')")
-    public String moderatorAccess() {
-        return "Moderator Board.";
-    }*/
+    @GetMapping("/*")
+    public String getUserInfo(Principal user) {
+        StringBuffer userInfo= new StringBuffer();
+        if (user instanceof UsernamePasswordAuthenticationToken){
+            userInfo.append(getUsernamePasswordLoginInfo(user));
+        }
+        else if (user instanceof OAuth2AuthenticationToken){
+            userInfo.append(getOauth2LoginInfo(user));
+        }
+        return userInfo.toString();
+
+    }
+
+    private StringBuffer getOauth2LoginInfo(Principal user){
+        StringBuffer protectedInfo = new StringBuffer();
+        OAuth2AuthenticationToken authToken = ((OAuth2AuthenticationToken) user);
+
+        OAuth2AuthorizedClient authClient = this.authorizedClientService
+                .loadAuthorizedClient(authToken.getAuthorizedClientRegistrationId(), authToken.getName());
+        Map<String, Object> userDetails = ((DefaultOAuth2User) authToken.getPrincipal()).getAttributes();
+
+        String userToken = authClient.getAccessToken().getTokenValue();
+        protectedInfo.append("bienvenu, " + userDetails.get("name")+"<br><br>");
+        protectedInfo.append("email, " + userDetails.get("email")+"<br><br>");
+        protectedInfo.append("Access Token, " + userToken +"<br><br>");
+        return protectedInfo;
+    }
+
+    private StringBuffer getUsernamePasswordLoginInfo(Principal user) {
+        StringBuffer usernameInfo = new StringBuffer();
+        UsernamePasswordAuthenticationToken token = ((UsernamePasswordAuthenticationToken) user);
+        if (token.isAuthenticated()){
+            User u =(User) token.getPrincipal();
+            usernameInfo.append("bienvenu, " + u.getUsername());
+        }
+        else {
+            usernameInfo.append("Na");
+        }
+
+        return usernameInfo;
+    }
+
 
     @GetMapping("/admin")
     @PreAuthorize("hasRole('ADMIN')")
